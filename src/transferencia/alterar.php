b04662dd45f9b776d640babbb177cc0e655c1261
Cleo Garcia email = cleonice.garcia@pmm.am.gov.br <roger@.(none)>
2013-07-08 10:51:18 -0400

    alteracao na tela de transferencia

--- src/transferencia/alterar.php
+++ src/transferencia/alterar.php
@@ -24,15 +24,15 @@ if ($_SESSION['ID_TRANSFERENCIA_ESTAGIO']) {
 
     $recrutamento = $VO->verificarRecrutamento();
 
-    /* if ($VO->ID_ORGAO_SOLICITANTE) { //if ($VO->ID_ORGAO_ESTAGIO) 
-      $VO->buscarAgenciaEstagio();
+    /*if ($VO->ID_ORGAO_SOLICITANTE) { //if ($VO->ID_ORGAO_ESTAGIO) 
+      $VO->pesquisarOrgaoCedente();
       $smarty->assign("arraypesquisarOrgaoCedente", $VO->getArray("TX_ORGAO_ESTAGIO"));
 
       if ($VO->ID_ORGAO_ESTAGIO) {  //if ($VO->ID_AGENCIA_ESTAGIO)
       $VO->buscarQuadroVagasEstagio();
       $smarty->assign("arrayQuadroVagasEstagio", $VO->getArray("TX_CODIGO"));
       }
-      } */
+      }*/
 
     if ($_POST) {
         $VO->configuracao();
