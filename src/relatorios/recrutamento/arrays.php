eb801072924397c817e8963b29f18f80a0167bc9
Smith Eduardo <smith.junior@pmm.am.gov.br>
2013-08-29 08:24:36 -0400

    relatorio

--- src/relatorios/recrutamento/arrays.php
+++ src/relatorios/recrutamento/arrays.php
@@ -1,7 +1,25 @@
 <?php
+require_once "../../php/define.php";
+require_once $pathvo."recrutamentoVO.php";
+	
+$VO = new recrutamentoVO();
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$VO->pesquisarOrgaoGestor();
+    $arrayOrgaoGestor = $VO->getArray("TX_ORGAO_GESTOR_ESTAGIO");  
+	
+/*$VO->pesquisarOrgaoSolicitante();
+    $arrayOrgaoSolicitante = $VO->getArray("TX_ORGAO_ESTAGIO");  */
+
+/*$VO->pesquisarQuadroVagas();
+    $arrayQuadroVagas = $VO->getArray("TX_CODIGO");    */
+
+$arraySituacao[''] 	= 'Escolha...';
+$arraySituacao[1] 	= 'Aberto';
+$arraySituacao[2] 	= 'Fechado';
+
+/*$smarty->assign("arrayQuadroVagas"    	    , $arrayQuadroVagas);
+$smarty->assign("arrayOrgaoSolicitante"    	, $arrayOrgaoSolicitante);*/
+$smarty->assign("arrayOrgaoGestor"          , $arrayOrgaoGestor);
+$smarty->assign("arraySituacao"			    , $arraySituacao);
+
 ?>
