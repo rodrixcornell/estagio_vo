942059a7f30bc2eefbfa12680a44647ebd2e7798
Rodrigo Cabral <rodrigo.cabral@pmm.am.gov.br>
2013-09-13 09:04:00 -0400

    modified:   js/quadro_vagas.js 	modified:   src/recesso/arrays.php 	modified:   templates/recesso/index.tpl 	modified:   templates/transferencia/cadastrar.tpl

--- js/quadro_vagas.js
+++ js/quadro_vagas.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
 
     function showLoader(){
         $('.fundo_pag').fadeIn(200);
-    }
+    };
 
     function hideLoader(){
         $('.fundo_pag').fadeOut(200);
     };
-	
+
 
     //---------------------pequisa--------------------------------------------------
     $('#pesquisar').click(function(){
-       
+
         //if(($('#ID_QUADRO_VAGAS_ESTAGIO').val() || $('#ID_ORGAO_GESTOR_ESTAGIO').val()) || ($('#ID_AGENCIA_ESTAGIO').val() || $('#CS_SITUACAO').val())){
             showLoader();
             $('#tabela').load('acoes.php?identifier=tabela',{
@@ -19,13 +19,13 @@ $(document).ready(function(){
                 ID_ORGAO_GESTOR_ESTAGIO:$('#ID_ORGAO_GESTOR_ESTAGIO').val(),
                 ID_AGENCIA_ESTAGIO:$('#ID_AGENCIA_ESTAGIO').val(),
                 CS_SITUACAO:$('#CS_SITUACAO').val()
-                },hideLoader);    
+                },hideLoader);
         //}else
         //    alert('Preencha pelo menos um campo para realizar a pesquisa!');
-  
+
     });
 
-   
+
     //--------------------Paginacao-------------------------------------------------
     $("#paginacao li").live('click', function(){
         showLoader();
